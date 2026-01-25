6a82cda74e7ac2c7220f53223a1f62e4ce69e22b
Lev Koliadich <lkolyadich@gmail.com>
2026-01-25 18:07:59 +0100

    Improve graph

--- src/btr_backup/commands/graph.py
+++ src/btr_backup/commands/graph.py
@@ -1,3 +1,5 @@
+from collections.abc import Generator
+from enum import StrEnum
 from pathlib import Path
 from typing import Any
 
@@ -5,12 +7,85 @@ from btr_backup.log import logger
 from btr_backup.protocols import Subparsers
 
 
-def make_header(logic_dir: str) -> str:
-    return f"┣━━ {logic_dir} ━━┓"
+class GraphElement(StrEnum):
+    newline = "\n"
+    whitespace = " "
+    trunk = "┃"
+    fork_right = "┣━━"
+    trun_down = "━━┓"
+    trun_right = "┗━━"
 
 
-def make_subvolume_line(subvolume: str, left_padding: int) -> str:
-    return f"┃{' ' * (left_padding - 2)}┣━━ {subvolume}"
+def generate_graph(logical_dirs: dict[str, list[str]]) -> Generator[str, None, None]:
+    items = list(logical_dirs.items())
+
+    for logic_dir, subvolumes in items[:-1]:
+        yield GraphElement.trunk
+        yield GraphElement.newline
+
+        yield GraphElement.fork_right
+        yield GraphElement.whitespace
+        yield logic_dir
+        yield GraphElement.whitespace
+        yield GraphElement.trun_down
+        yield GraphElement.newline
+
+        padding_len = (
+            len(GraphElement.fork_right)
+            + len(GraphElement.whitespace)
+            + len(logic_dir)
+            + len(GraphElement.whitespace)
+            + len(GraphElement.trun_down)
+            - 1  # trunk
+            - 1  # start at the same column
+        )
+        for subvolume in subvolumes[:-1]:
+            yield GraphElement.trunk
+            yield GraphElement.whitespace * padding_len
+            yield GraphElement.fork_right
+            yield GraphElement.whitespace
+            yield subvolume
+            yield GraphElement.newline
+
+        yield GraphElement.trunk
+        yield GraphElement.whitespace * padding_len
+        yield GraphElement.trun_right
+        yield GraphElement.whitespace
+        yield subvolumes[-1]
+        yield GraphElement.newline
+
+    logic_dir, subvolumes = items[-1]
+
+    yield GraphElement.trunk
+    yield GraphElement.newline
+
+    yield GraphElement.trun_right
+    yield GraphElement.whitespace
+    yield logic_dir
+    yield GraphElement.whitespace
+    yield GraphElement.trun_down
+    yield GraphElement.newline
+
+    padding_len = (
+        len(GraphElement.fork_right)
+        + len(GraphElement.whitespace)
+        + len(logic_dir)
+        + len(GraphElement.whitespace)
+        + len(GraphElement.trun_down)
+        - 1  # start at the same column
+    )
+    for subvolume in subvolumes[:-1]:
+        yield GraphElement.whitespace * padding_len
+        yield GraphElement.fork_right
+        yield GraphElement.whitespace
+        yield subvolume
+        yield GraphElement.newline
+
+    yield GraphElement.whitespace * padding_len
+    yield GraphElement.trun_right
+    yield GraphElement.whitespace
+    yield subvolumes[-1]
+    yield GraphElement.newline
 
 
 def graph_subvolumes(working_dir: Path, **kwargs: Any) -> None:
@@ -25,12 +100,7 @@ def graph_subvolumes(working_dir: Path, **kwargs: Any) -> None:
 
         logical_volumes[logic_dir.name] = sorted(subvolumes, reverse=True)
 
-    for logic_dir, subvolumes in logical_volumes.items():
-        header = make_header(logic_dir)
-        print(header)
-
-        for subvolume in subvolumes:
-            print(make_subvolume_line(subvolume, len(header)))
+    print("".join(generate_graph(logical_volumes)))
 
 
 def add_command(subparsers: Subparsers) -> None:
